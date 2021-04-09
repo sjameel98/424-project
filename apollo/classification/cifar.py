3762df7bab75de7268c3b94f95f420a649ca583c
arifmoh2 <mustafa.arif@mail.utoronto.ca>
2021-04-09 18:04:57 -0400

    transformation change for omniglot

--- apollo/classification/cifar.py
+++ apollo/classification/cifar.py
@@ -189,6 +189,8 @@ def setup(args):
                        ]))
         valset = dataset(data_path, train=False, download=False, split='byclass',
                         transform=transforms.Compose([
+                            transforms.RandomCrop(32, padding=4),
+                            transforms.RandomHorizontalFlip(),
                             transforms.ToTensor(),
                             transforms.Normalize((0.4914), (0.2023)),
                         ]))
